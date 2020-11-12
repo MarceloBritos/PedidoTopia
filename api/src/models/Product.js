11c4971bed32faa182bc4c23c5e876642b02385e
Marcelo Britos <66213495+MarceloBritos@users.noreply.github.com>
2020-11-12 11:17:52 -0300

    Add files via upload

---
+++ api/src/models/Product.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("product", {
+    title: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+    },
+    sku: {
+      type: DataTypes.STRING,
+      // allowNull: false,
+    },
+    stock_inicial: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    stock_total_actual: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    precio_inicial: {
+      type: DataTypes.REAL,
+      defaultValue: 0
+    },
+    images: {
+      type: DataTypes.ARRAY(DataTypes.TEXT)
+    },
+    productId_Shopify: {
+      type: DataTypes.BIGINT,
+      defaultValue: null
+    },
+    productId_Meli: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+  });
+};
